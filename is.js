6a15d3f4f61075fa55440ac7ec6b176c11da6215
Mikol Graves <mikol@thinbox.org>
2016-03-01 21:51:20 -0800

    Initial implementation.

---
+++ is.js
@@ -0,0 +1,235 @@
+/* jscs:disable maximumLineLength */
+(function (root, factory) { 'use strict'; var deps = [
+  'instance'
+]; if (typeof define === 'function' && define.amd) { define(deps, function () { return factory.apply(root, [].slice.call(arguments)); }); } else if (typeof module === 'object' && module.exports) { for (var x = deps.length; x--;) { deps[x] = require(deps[x]); } module.exports = factory.apply(root, deps); }/* else { TODO: Default? } */}((typeof global === 'object' && global) || (typeof window === 'object' && window) || /* jshint -W040 */this/* jshint +W040 */,
+/* jscs:enable maximumLineLength */
+
+function (instance) {
+  'use strict';
+
+  var stringify = {}.toString.call.bind({}.toString);
+
+  /**
+   * @const {null}
+   * @private
+   */
+  var N = null;
+
+  /**
+   * @const {undefined}
+   * @private
+   */
+  var U;
+
+  return instance.create(N, {
+    /**
+     * Determines if `v` is an array.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is an array; `false` otherwise.
+     */
+    array: function isArray(v) {
+      return v != N &&
+          stringify(v) === '[object Array]';
+    },
+
+    /**
+     * Determines if `v` is a boolean.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is a boolean; `false` otherwise.
+     */
+    boolean: function isBoolean(v) {
+      return v != N &&
+          typeof v === 'boolean' || stringify(v) === '[object Boolean]';
+    },
+
+    /**
+     * Determines if `v` is a date.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is a date; `false` otherwise.
+     */
+    date: function isDate(v) {
+      return v != N &&
+          stringify(v) === '[object Date]';
+    },
+
+    /**
+     * Determines if `v` is an error.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is an error; `false` otherwise.
+     */
+    error: function isError(v) {
+      return v != N &&
+          stringify(v) === '[object Error]';
+    },
+
+    /**
+     * Determines if `v` has a meaningful, introspectable value (that is, not
+     * the non-value `null` and not the unitialized value `undefined`).
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` has a meaningful value;
+     *     `false` otherwise.
+     *
+     * @see is.{@link module:is~ext|ext}()
+     * @see is.{@link module:is~nix|nix}()
+     */
+    extent: function isExtent(v) {
+      return v != N;
+    },
+
+    /**
+     * Determines if `v` is a function.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is a function; `false` otherwise.
+     */
+    function: function isFunction(v) {
+      return v != N &&
+          typeof v === 'function';
+    },
+
+    /**
+     * Determines if `v` is *not* a usable value (that is, the non-value `null`
+     * or the unitialized value `undefined`); the opposite of `is.extent()`.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is not a usable value;
+     *     `false` otherwise.
+     *
+     * @see is.{@link module:is~extent|extent}()
+     */
+    nix: function isNix(v) {
+      return v == N;
+    },
+
+    /**
+     * Determines if `v` is `null`.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is `null`; `false` otherwise.
+     */
+    null: function isNull(v) {
+      return v === N;
+    },
+
+    /**
+     * Determines if `v` is a number.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is a number; `false` otherwise.
+     */
+    number: function isNumber(v) {
+      return v != N &&
+          !isNaN(v) && stringify(v) === '[object Number]';
+    },
+
+    /**
+     * Determines if `v` is a non-primitive, non-null type; not `boolean`,
+     * `number`, `string`, `null`, or `undefined`, but any other data type that
+     * can be extended dynamically with properties and methods such as the
+     * primitive wrappers `Boolean`, `Number`, `String`, as well as built-in
+     * types like `Array`, `Date`, `Function`, `Object`, `RegExp`, and others.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is an object; `false` otherwise.
+     */
+    object: function isObject(v) {
+      if (v != N) {
+        var tov = typeof v;
+        return tov === 'object' || tov === 'function';
+      }
+
+      return false;
+    },
+
+    /**
+     * Determines if `v` is a Promises/A+ promise instance from the current
+     * (i.e., global `Promise` constructor) implementation.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is an instance of the current
+     *     Promises/A+ implementation; `false` otherwise.
+     */
+    promise: function isPromise(v) {
+      return v != N &&
+          v.constructor === Promise.resolve().constructor;
+    },
+
+    /**
+     * Determines if `v` is a regular expression.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is a regular expression; `false`
+     *     otherwise.
+     */
+    regexp: function isRegExp(v) {
+      return v != N &&
+          stringify(v) === '[object RegExp]';
+    },
+
+    /**
+     * Determines if `v` is a string.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is a string; `false` otherwise.
+     */
+    string: function isString(v) {
+      return v != N &&
+          typeof v === 'string' || stringify(v) === '[object String]';
+    },
+
+    /**
+     * Determines if `v` is a symbol.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` is a symbol; `false` otherwise.
+     */
+    symbol: function isSymbol(v) {
+      return v != N &&
+          typeof v === 'symbol' || stringify(v) === '[object Symbol]';
+    },
+
+    /**
+     * Determines if `v` exposes a Promises/A+-compliant `then` method.
+     *
+     * @param {*} v - The value to test.
+     *
+     * @return {!boolean} `true` if `v` implements a `then` method;
+     *     `false` otherwise.
+     */
+    thenable: function isThenable(v) {
+      return v != N && typeof v.then === 'function';
+    },
+
+    /**
+     * Determines if `v` is `undefined`.
+     *
+     * @param {*} v The value to test.
+     *
+     * @return {!boolean} `true` if `v` is `undefined`; `false` otherwise.
+     */
+    undefined: function isUndefined(v) {
+      return v === U;
+    },
+  });
+
+  // TODO: Implement terse aliases?
+}));
